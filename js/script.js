6f2b3342bb562a61d3491cc4d548bb86c2be9246
Chyngys <chynga2002@mail.ru>
2020-04-05 14:12:43 +0600

    calculator

--- js/script.js
+++ js/script.js
@@ -203,4 +203,55 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    // Calculator
+
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+        totalValue.innerHTML = 0;
+
+        persons.addEventListener('input', function() {
+            personsSum = +this.value;
+            total = (daysSum + personsSum) * 4000;
+
+            if(restDays.value == '') {
+                totalValue.innerHTML = 0;
+            } else {
+                totalValue.innerHTML = total;
+            }
+            
+            if (persons.value == '') {
+                totalValue.innerHTML = 0;
+            }
+        });
+
+        restDays.addEventListener('input', function() {
+            daysSum = +this.value;
+            total = (daysSum + personsSum) * 4000;
+
+            if(persons.value == '') {
+                totalValue.innerHTML = 0;
+            } else {
+                totalValue.innerHTML = total;
+            }
+
+            if (restDays.value == '') {
+                totalValue.innerHTML = 0;
+            }
+        });
+
+        place.addEventListener('input', function() {
+            if(persons.value == '' || restDays.value == '') {
+                totalValue.innerHTML = 0;
+            } else {
+                let a = total;
+                totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+            }
+        });
 });
